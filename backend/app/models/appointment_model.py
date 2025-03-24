5ed9ba09146f5a8f10a454429336d3a017a77480
shruti-doon <shruti23cc12@gmail.com>
2025-03-24 22:15:48 +0530

    dev

--- backend/app/models/appointment_model.py
+++ backend/app/models/appointment_model.py
@@ -2,9 +2,14 @@ from pymongo import MongoClient
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
+from urllib.parse import quote_plus
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["therapist_dashboard"]
+username = quote_plus('shruti23doon')
+password = quote_plus('Shruti@23')
+dbname = 'therapist_dashboard'
+MONGODB_URI = f'mongodb+srv://{username}:{password}@patientdata.1807l.mongodb.net/{dbname}?retryWrites=true&w=majority'
+client = MongoClient(MONGODB_URI)
+db = client[dbname]
 appointments_collection = db["appointments"]
 
 class Appointment(BaseModel):
@@ -13,6 +18,6 @@ class Appointment(BaseModel):
     age: str
     email: str
     notes: str
-    status: str  # "today", "upcoming", or "completed"
+    status: str 
     time: str
     date: str
